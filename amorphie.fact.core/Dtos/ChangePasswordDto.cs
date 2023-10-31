24cc142617f930a6ee33115285d96d1f0e82a30d
tsimsekburgan <TSimsek@burgan.com.tr>
2023-10-31 18:22:10 +0300

    Change Ps For Flutter Render Test

---
+++ amorphie.fact.core/Dtos/ChangePasswordDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+    public class ChangePasswordDto
+    {
+        public string? oldPassword {get;set;}
+        public string? newPassword {get;set;}
+        public string? newPasswordConfirm {get;set;}
+    }
