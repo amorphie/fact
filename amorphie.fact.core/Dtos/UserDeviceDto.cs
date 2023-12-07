c55f26d5e56561513d9de029b5038eed9bb52521
Sercan Yüksel <srcanyuksel@gmail.com>
2023-12-07 16:31:52 +0300

    fix

--- amorphie.fact.core/Dtos/UserDeviceDto.cs
+++ amorphie.fact.core/Dtos/UserDeviceDto.cs
@@ -2,7 +2,7 @@ using amorphie.core.Base;
 
 public class UserDeviceDto: DtoBase
 {
-    public int DeviceId { get; set; }
+    public Guid DeviceId { get; set; }
     public Guid TokenId { get; set; }
     public Guid ClientId { get; set; }   
     public Guid UserId { get; set; }
