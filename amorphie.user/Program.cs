008475f59efb7f2bdfbd78d85355b2993609641a
Sercan Yüksel <srcanyuksel@gmail.com>
2024-02-12 03:12:20 +0300

    fix

--- amorphie.user/Program.cs
+++ amorphie.user/Program.cs
@@ -4,8 +4,23 @@ using amorphie.core.Identity;
 using System.Reflection;
 using FluentValidation;
 using amorphie.core.Extension;
+using Dapr.Client;
 
 var builder = WebApplication.CreateBuilder(args);
+var client = new DaprClientBuilder().Build();
+using (var tokenSource = new CancellationTokenSource(20000))
+{
+    try
+    {
+        await client.WaitForSidecarAsync(tokenSource.Token);
+    }
+    catch (System.Exception)
+    {
+        Console.WriteLine("Dapr Sidecar Doesn't Respond");
+        return;
+    }
+    
+}
 await builder.Configuration.AddVaultSecrets("user-secretstore", new string[] { "user-secretstore" });
 var postgreSql = builder.Configuration["postgresql"];
 // var postgreSql = "Host=localhost:5432;Database=users;Username=postgres;Password=postgres";
