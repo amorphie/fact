2b4aff040632e71125ddda1e64ded3edbfd788bf
Sercan Yüksel <srcanyuksel@gmail.com>
2024-04-16 15:18:21 +0300

    dto fix

--- amorphie.fact.core/Dtos/ClientDto.cs
+++ amorphie.fact.core/Dtos/ClientDto.cs
@@ -26,6 +26,8 @@ public class ClientDto : DtoBase
     public ICollection<ClientGrantType> AllowedGrantTypes { get; set; } = default!;
     public ICollection<ClientFlow> Flows { get; set; } = default!;
     public ICollection<ClientAudience> Audiences { get; set; } = default!;
+    public bool CanCreateLoginUrl{get;set;} = default!;
+    public string[]? CreteLoginUrlClients { get; set; } = default!;
     public string? JwtSecretSalt { get; set; }
 }
 
